b67908c3d71aba41c0d8dbfb078bd2ab03c225e8
Samson Magesh (mageshs) <mageshs@coventry.ac.uk>
2021-02-10 12:16:19 +0000

    Modifying files to try and figure out the error. Re added some code to see if that fixes the problem

--- 2b Memory/LoadAllData.m
+++ 2b Memory/LoadAllData.m
@@ -1,10 +1,12 @@
-
 function [AllDataMem] = LoadAllData(FileName)
 %% Section 2: Load all the model data together
+
 Contents = ncinfo(FileName); % Store the file content information in a variable.
+
 for idx = 1: 8
- AllData(idx,:,:,:) = ncread(FileName, Contents.Variables(idx).Name);
- fprintf('Loading %s\n', Contents.Variables(idx).Name); % display loading information
+    AllData(idx,:,:,:) = ncread(FileName, Contents.Variables(idx).Name);
+    fprintf('Loading %s\n', Contents.Variables(idx).Name); % display loading information
 end
+
 AllDataMem = whos('AllData').bytes/1000000;
 fprintf('Memory used for all data: %.3f MB\n', AllDataMem)
--- 2b Memory/MemorySaving.m
+++ 2b Memory/MemorySaving.m
@@ -3,24 +3,13 @@ clear all % clear all variables
 close all % close all windows
 
 FileName = '..\Model\o3_surface_20180701000000.nc'; % define the name of the file to be used, the path is included
-
-
-
 Contents = ncinfo(FileName); % Store the file content information in a variable.
-
-
-% %% Section 2: Load all the model data together
-% for idx = 1: 8
-%     AllData(idx,:,:,:) = ncread(FileName, Contents.Variables(idx).Name);
-%     fprintf('Loading %s\n', Contents.Variables(idx).Name); % display loading information
-% end
-% 
-% AllDataMem = whos('AllData').bytes/1000000;
-% fprintf('Memory used for all data: %.3f MB\n', AllDataMem)
-
 [AllDataMem] = LoadAllData(FileName);
 [HourDataMem] = LoadHours(FileName);
-[HourMem] = LoadAllHours(FileName);
+[HourMem]= LoadAllHours(FileName);
 ReportResults(AllDataMem,HourDataMem,HourMem);
 
- 
+
+
+
+
