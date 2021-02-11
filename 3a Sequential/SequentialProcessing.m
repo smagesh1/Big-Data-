7676ccdf781f1507a4935de4280c3c3bc2f8583b
Samson Magesh (mageshs) <mageshs@coventry.ac.uk>
2021-02-11 19:10:44 +0000

    Changing the hours to test different hours, whether it has an impact on the time or not

--- 3a Sequential/SequentialProcessing.m
+++ 3a Sequential/SequentialProcessing.m
@@ -24,7 +24,7 @@ NumLat = 400; % number of latitude locations ot load
 StartLon = 1; % longitude location to start loading
 NumLon = 700; % number of longitude locations ot load
 tic
-for NumHour = 1:1 % loop through each hour
+for NumHour = 1:4 % loop through each hour
     fprintf('Processing hour %i\n', NumHour)
     DataLayer = 1; % which 'layer' of the array to load the model data into
     for idx = [1, 2, 4, 5, 6, 7, 8] % model data to load
