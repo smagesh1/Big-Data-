ca0ea22f97cf60e2807d3300372170b6876cff1a
Samson Magesh (mageshs) <mageshs@coventry.ac.uk>
2021-03-12 18:40:53 +0000

    Changing the location of the data file

--- 6b Testing NaN/CreateTestData_NaN.m
+++ 6b Testing NaN/CreateTestData_NaN.m
@@ -2,7 +2,8 @@
 clear all
 close all
 
-OriginalFileName = './Model/o3_surface_20180701000000.nc';
+%%OriginalFileName = './Model/o3_surface_20180701000000.nc';
+OriginalFileName = 'C:\Users\mages\OneDrive\Documents\5011CEM\5011CEM2021_mageshs\Model\o3_surface_20180701000000.nc';
 NewFileName = './Model/TestFileNaN.nc';
 copyfile(OriginalFileName, NewFileName);
 
