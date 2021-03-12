5ac727f1d7ea0f10aa54a40ed2c8faa1f20b56e4
Samson Magesh (mageshs) <mageshs@coventry.ac.uk>
2021-03-12 18:21:43 +0000

    Changed the FileIn location to the where my data file is located

--- 6a Testing Text/CreateTestData_Text.m
+++ 6a Testing Text/CreateTestData_Text.m
@@ -2,7 +2,8 @@
 clear all
 close all
 
-FileIn = '.\Model\o3_surface_20180701000000.nc';
+%%FileIn = '.\Model\o3_surface_20180701000000.nc';
+FileIn = 'C:\Users\mages\OneDrive\Documents\5011CEM\5011CEM2021_mageshs\Model\o3_surface_20180701000000.nc';
 C = ncinfo(FileIn);
 VarNames = {C.Variables.Name};
 
