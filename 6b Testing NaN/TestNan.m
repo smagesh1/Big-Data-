daad61229ebc4ad17c7b303fa3e39915ca0133c7
Samson Magesh (mageshs) <mageshs@coventry.ac.uk>
2021-03-12 18:27:23 +0000

    Changing the FileName variable location of the data file to the location of where it is located on my computer

--- 6b Testing NaN/TestNan.m
+++ 6b Testing NaN/TestNan.m
@@ -9,7 +9,9 @@ close all
 %% Test a good file
 NaNErrors = 0;
 %% Set file to test
-FileName = '../Model/o3_surface_20180701000000.nc'; % define our test file
+%%FileName = '../Model/o3_surface_20180701000000.nc'; % define our test file
+
+FileName = 'C:\Users\mages\OneDrive\Documents\5011CEM\5011CEM2021_mageshs\Model\o3_surface_20180701000000.nc';
 
 Contents = ncinfo(FileName); % Store the file content information in a variable.
 
