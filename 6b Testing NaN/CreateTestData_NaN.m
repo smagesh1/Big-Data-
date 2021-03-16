b679071757ccf30bc45ea56e757c696606c83026
Samson Magesh (mageshs) <mageshs@coventry.ac.uk>
2021-03-16 13:05:45 +0000

    Changed the path for the TestfileNaN

--- 6b Testing NaN/CreateTestData_NaN.m
+++ 6b Testing NaN/CreateTestData_NaN.m
@@ -4,7 +4,7 @@ close all
 
 %%OriginalFileName = './Model/o3_surface_20180701000000.nc';
 OriginalFileName = 'C:\Users\mages\OneDrive\Documents\5011CEM\5011CEM2021_mageshs\Model\o3_surface_20180701000000.nc';
-NewFileName = './Model/TestFileNaN.nc';
+NewFileName = 'C:\Users\mages\OneDrive\Documents\5011CEM\5011CEM2021_mageshs\Model\TestFileNaN.nc';
 copyfile(OriginalFileName, NewFileName);
 
 C = ncinfo(NewFileName);
