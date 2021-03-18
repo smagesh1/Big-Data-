0d50a0093dc2f1d1693a9aaa956c61c1068fa5e3
Samson Magesh (mageshs) <mageshs@coventry.ac.uk>
2021-03-18 16:57:26 +0000

    Changed the model value

--- 6a Testing Text/CreateTestData_Text.m
+++ 6a Testing Text/CreateTestData_Text.m
@@ -4,6 +4,7 @@ close all
 
 %%FileIn = '.\Model\o3_surface_20180701000000.nc';
 FileIn = 'C:\Users\mages\OneDrive\Documents\5011CEM\5011CEM2021_mageshs\Model\o3_surface_20180701000000.nc';
+
 C = ncinfo(FileIn);
 VarNames = {C.Variables.Name};
 
@@ -20,7 +21,7 @@ ncwrite(FileOut, 'lon', ncread(FileIn, 'lon'));
 nccreate(FileOut, 'hour', 'Dimensions', {'hour', 25}, 'DataType', 'single');
 ncwrite(FileOut, 'hour', ncread(FileIn, 'hour'));
 
-Model2Change = 6; % Select the model that will be overwritten with errors
+Model2Change = 5; % Select the model that will be overwritten with errors
 
 for idx = 1:7
     if idx ~= Model2Change 
