c248f65d48769f26693903ca0202305246788337
Samson Magesh (mageshs) <mageshs@coventry.ac.uk>
2021-03-26 12:52:27 +0000

    Changing file paths

--- 6a Testing Text/CreateTestData_Text.m
+++ 6a Testing Text/CreateTestData_Text.m
@@ -3,7 +3,10 @@ clear all
 close all
 
 %%FileIn = '.\Model\o3_surface_20180701000000.nc';
-FileIn = 'C:\Users\mages\OneDrive\Documents\5011CEM\5011CEM2021_mageshs\Model\o3_surface_20180701000000.nc';
+%%FileIn = 'C:\Users\mages\OneDrive\Documents\5011CEM\5011CEM2021_mageshs\Model\o3_surface_20180701000000.nc';
+
+FileIn =  'C:\Users\Samson\Documents\GitHub\5011CEM2021_mageshs\Model\o3_surface_20180701000000.nc';
+
 
 C = ncinfo(FileIn);
 VarNames = {C.Variables.Name};
--- 6a Testing Text/TestText.m
+++ 6a Testing Text/TestText.m
@@ -8,7 +8,8 @@ DataTypes = {'NC_Byte', 'NC_Char', 'NC_Short', 'NC_Int', 'NC_Float', 'NC_Double'
 
 %% Test a good file
 %% Set file to test
-FileName = '../Model/o3_surface_20180701000000.nc'; % define our test file
+%%FileName = '../Model/o3_surface_20180701000000.nc'; % define our test file
+FileName = 'C:\Users\Samson\Documents\GitHub\5011CEM2021_mageshs\Model\o3_surface_20180701000000.nc';
 
 Contents = ncinfo(FileName); % Store the file content information in a variable.
 FileID = netcdf.open(FileName,'NC_NOWRITE'); % open file read only and create handle
@@ -37,8 +38,9 @@ end
 %% Test File with Errors
 %% Set file to test;
     %%FileName = '../Model/TestFileText.nc'; % define our test file
-    FileName = 'C:\Users\mages\OneDrive\Documents\5011CEM\5011CEM2021_mageshs\6a Testing Text\TestyTest.nc';
-
+    %%FileName = 'C:\Users\mages\OneDrive\Documents\5011CEM\5011CEM2021_mageshs\6a Testing Text\TestyTest.nc';
+    FileName = 'C:\Users\Samson\Documents\GitHub\5011CEM2021_mageshs\6a Testing Text\TestyTest.nc';
+    
     Contents = ncinfo(FileName); % Store the file content information in a variable.
     FileID = netcdf.open(FileName,'NC_NOWRITE'); % open file read only and create handle
 
