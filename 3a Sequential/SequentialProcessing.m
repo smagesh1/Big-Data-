e3b99f3713cc728576af5d721c26f6fa393c60db
Samson Magesh (mageshs) <mageshs@coventry.ac.uk>
2021-03-30 19:35:44 +0100

    Removed the function and commentated out the clear statements

--- 3a Sequential/SequentialProcessing.m
+++ 3a Sequential/SequentialProcessing.m
@@ -1,6 +1,6 @@
 %% This script allows you to open and explore the data in a *.nc file
-clear all
-close all
+%clear all
+%close all
 
 FileName = 'C:\Users\Samson\Documents\GitHub\5011CEM2021_mageshs\Model\o3_surface_20180701000000.nc';
 
@@ -24,21 +24,16 @@ NumLat = 400; % number of latitude locations ot load
 StartLon = 1; % longitude location to start loading
 NumLon = 700; % number of longitude locations ot load
 tic
-% for NumHour = 1:5 % loop through each hour
-%     fprintf('Processing hour %i\n', NumHour)
-%     DataLayer = 1; % which 'layer' of the array to load the model data into
-%     for idx = [1, 2, 4, 5, 6, 7, 8] % model data to load
-%         % load the model data
-%         HourlyData(DataLayer,:,:) = ncread(FileName, Contents.Variables(idx).Name,...
-%             [StartLon, StartLat, NumHour], [NumLon, NumLat, 1]);
-%         DataLayer = DataLayer + 1; % step to the next 'layer'
-%     end
-Options = [1,2,3,4,5]
-for NumHour = 1:size(Options,1)
+for NumHour = 1:5 % loop through each hour
     fprintf('Processing hour %i\n', NumHour)
-    for idx = 1:size(Options,1)
-        Results(idx,:) = [Options(idx),sum(T2)];
+    DataLayer = 1; % which 'layer' of the array to load the model data into
+    for idx = [1, 2, 4, 5, 6, 7, 8] % model data to load
+        % load the model data
+        HourlyData(DataLayer,:,:) = ncread(FileName, Contents.Variables(idx).Name,...
+            [StartLon, StartLat, NumHour], [NumLon, NumLat, 1]);
+        DataLayer = DataLayer + 1; % step to the next 'layer'
     end
+
     % We need to prepare our data for processing. This method is defined by
     % our customer. You are not required to understand this method, but you
     % can ask your module leader for more information if you wish.
