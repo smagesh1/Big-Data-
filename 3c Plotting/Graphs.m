cee62ce3c41d277c88543cfa3da345399cdc80ad
Samson Magesh (mageshs) <mageshs@coventry.ac.uk>
2021-04-15 17:27:44 +0100

    Plotting 5000 dataset results on the same graph as 10,000

--- 3c Plotting/Graphs.m
+++ 3c Plotting/Graphs.m
@@ -7,13 +7,23 @@ close all
 x2Vals = [2, 3, 4, 5, 6];
 y2Vals = [401.76 , 273.78 , 257.94 , 204.84 , 187.67];
 figure(1)
-yyaxis right
-plot(x2Vals, y2Vals, '-rx')
+yyaxis left
+plot(x2Vals, y2Vals, '-bd')
 xlabel('Number of Processors')
 ylabel('Processing time (s)')
 title('Processing time vs number of processors')
 
-legend('10,000 Data')
+%% 5,000 data processed
+x3Vals = [2, 3, 4, 5, 6];
+y3Vals = [203.82 , 146.50 , 109.94 , 95.16 , 90.89];
+figure(1)
+yyaxis right
+plot(x3Vals, y3Vals, '-rx')
+xlabel('Number of Processors')
+ylabel('Processing time (s)')
+title('Processing time vs number of processors')
+
+legend('5,000 Data','10,000 Data')
 
 
 %% Show two plots on same y-axis
@@ -28,14 +38,3 @@ ylabel('Processing time (s)')
 title('Mean Processing time vs number of processors')
 legend('10,000 Data')
 
-%% 5,000 data processed
-x3Vals = [2, 3, 4, 5, 6];
-y3Vals = [203.82 , 146.50 , 109.94 , 95.16 , 90.89];
-figure(1)
-yyaxis right
-plot(x3Vals, y3Vals, '-rx')
-xlabel('Number of Processors')
-ylabel('Processing time (s)')
-title('Processing time vs number of processors')
-
-legend('5,000 Data')
