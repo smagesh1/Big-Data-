28159008db5ff986ba98ffd44863614f13112a70
Samson Magesh (mageshs) <mageshs@coventry.ac.uk>
2021-04-15 19:32:39 +0100

    Adding mean vals for 5000 dataset to the same plot as 10000

--- 3c Plotting/Graphs.m
+++ 3c Plotting/Graphs.m
@@ -29,12 +29,16 @@ legend('5,000 Data','10,000 Data')
 %% Show two plots on same y-axis
 %% Mean processing time
 y2MeanVals = y2Vals / 10000;
+y3MeanVals = y3Vals/ 5000;
+
+
 
 figure(2)
+plot(x2Vals, y2MeanVals, '-bd')
 hold on
-plot(x2Vals, y2MeanVals, '-rx')
+plot(x3Vals,y3MeanVals, '-rx')
 xlabel('Number of Processors')
 ylabel('Processing time (s)')
 title('Mean Processing time vs number of processors')
-legend('10,000 Data')
+legend('5,000 Data','10,000 Data')
 
